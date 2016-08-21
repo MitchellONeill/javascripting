f7172e2d4184f951e62321730732d7fb12664e3e
Mitchell O'Neill <7moneill@gmail.com>
2016-08-21 13:57:41 -0700

    Javascripting assignment files, complete

---
+++ function-arguments.js
@@ -0,0 +1,7 @@
+
+function math (first, second, third) {
+  var value = second * third + first;
+  return value;
+}
+
+console.log(math(53, 61, 67));
---
+++ number-to-string.js
@@ -0,0 +1,4 @@
+var n = 128;
+var str = n.toString();
+console.log(str);
+
